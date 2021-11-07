9b210c49ce041f3bd311019beab755b9dbfe31ed
Jesse Nestler <jesse.nestler@colorado.edu>
2021-11-07 14:57:13 -0700

    Alter database schema
    Create more logical database schema centered around analysis workflow.

--- 0-Initialize/intialize_db.sql
+++ 0-Initialize/intialize_db.sql
@@ -1,5 +1,7 @@
--- Run this file as a superuser connected to the default database
+-- Run this file as a superuser connected to the user's database of choice
 create schema if not exists sandbox;
-create schema if not exists flood;
-set search_path = flood, sandbox;
+create schema if not exists raw;
+create schema if not exists proc;
+create schema if not exists fin;
+set search_path = sandbox, raw, proc, fin;
 create extension if not exists postgis;
